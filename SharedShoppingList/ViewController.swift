98a32a38f8459e64079282938cbfdfb65dff7f31
Dr. Guido Mocken <mocken@freenet.de>
2020-02-09 21:34:10 +0100

    Initial Commit

---
+++ SharedShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharedShoppingList
+//
+//  Created by Dr. Guido Mocken on 09.02.20.
+//  Copyright © 2020 Guido R. Mocken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
